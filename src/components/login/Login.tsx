0a54330cf7869c95d5a5328246074a6a6b70b98c
Opp Lieam <runicer156@gmail.com>
2024-07-16 03:47:54 +0700

    feat(login-form): add login form and validator

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -6,17 +6,47 @@ import {
   TextInput,
   Title
 } from "@mantine/core";
+import { hasLength, isEmail, useForm } from "@mantine/form";
 
 function Login() {
+  const form = useForm({
+    mode: "uncontrolled",
+    initialValues: {
+      email: "",
+      password: ""
+    },
+    validate: {
+      email: isEmail("Invalid Email"),
+      password: hasLength({ min: 8 }, "Invalid Password")
+    }
+  });
+
+  const submitHandler = values => {
+    console.log(values);
+  };
+
   return (
     <Container my={40} size={420}>
       <Title ta="center">Welcome back!</Title>
       <Paper withBorder shadow="md" p={30} mt={30} radius={"md"}>
-        <TextInput label="Email" required />
-        <PasswordInput label="Password" required mt="md" />
-        <Button fullWidth mt="xl">
-          Sign in
-        </Button>
+        <form onSubmit={form.onSubmit(values => submitHandler(values))}>
+          <TextInput
+            label="Email"
+            required
+            keys={form.key("email")}
+            {...form.getInputProps("email")}
+          />
+          <PasswordInput
+            label="Password"
+            required
+            mt="md"
+            keys={form.key("password")}
+            {...form.getInputProps("password")}
+          />
+          <Button fullWidth mt="xl" type="submit">
+            Sign in
+          </Button>
+        </form>
       </Paper>
     </Container>
   );
