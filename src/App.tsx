1149cc00d5d1b135dad913be37fb5300ae10221d
Opp Lieam <runicer156@gmail.com>
2024-07-16 02:00:09 +0700

    style(login): rename file

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { MantineProvider } from "@mantine/core";
-import Login from "./components/login/login.tsx";
+import Login from "./components/login/Login.tsx";
 
 const router = createBrowserRouter([
   {
