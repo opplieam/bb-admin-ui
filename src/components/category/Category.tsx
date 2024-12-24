6ed073730f8c20d5990f132735f3d55cb3304073
Opp Lieam <runicer156@gmail.com>
2024-12-24 10:13:20 +0700

    fix: category update

--- src/components/category/Category.tsx
+++ src/components/category/Category.tsx
@@ -1,6 +1,6 @@
 import { Button, Divider, Group } from "@mantine/core";
 import useGetAllUnmatchedCat, {
-  UnmatchedCat
+  UnmatchedCat,
 } from "./hooks/useGetAllUnmatchedCat.ts";
 import { useState } from "react";
 import { DataTable } from "mantine-datatable";
@@ -15,7 +15,7 @@ function Category() {
   >([]);
   const { data: resultUnmatched } = useGetAllUnmatchedCat({
     page: page,
-    page_size: pageSize
+    page_size: pageSize,
   });
 
   const { data: resultCategory } = useGetAllCategory();
@@ -24,9 +24,11 @@ function Category() {
   const { isPending, mutate } = useUpdateCategory();
 
   const handleUpdate = () => {
-    const unmatchedCatID = selectedUnmatchedCat.map(v => v.id);
+    const unmatchedCatID = selectedUnmatchedCat.map((v) => v.id);
     const catID = selectedCategory[0].id;
     mutate({ unmatched_category_id: unmatchedCatID, category_id: catID });
+    setSelectedCategory([]);
+    setSelectedUnmatchedCat([]);
   };
   // noinspection TypeScriptValidateTypes
   return (
@@ -37,14 +39,14 @@ function Category() {
         columns={[
           { accessor: "id" },
           { accessor: "path" },
-          { accessor: "category_level" }
+          { accessor: "category_level" },
         ]}
         noRecordsText="No records available"
         records={resultUnmatched?.data}
         page={page}
         recordsPerPage={pageSize}
         totalRecords={resultUnmatched?.metadata.total_records}
-        onPageChange={p => setPage(p)}
+        onPageChange={(p) => setPage(p)}
         selectedRecords={selectedUnmatchedCat}
         onSelectedRecordsChange={setSelectedUnmatchedCat}
       />
@@ -57,7 +59,7 @@ function Category() {
         records={resultCategory?.data}
         selectedRecords={selectedCategory}
         onSelectedRecordsChange={setSelectedCategory}
-        isRecordSelectable={record =>
+        isRecordSelectable={(record) =>
           record == selectedCategory[0] || selectedCategory.length < 1
         }
       />
